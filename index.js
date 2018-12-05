7dbabad62c16216b616fcaf5722113a06c9dc054
cchak <chrischakct@gmail.com>
2018-12-05 17:51:27 -0500

    prod/dev

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const keys = require("./config/keys");
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ hi: keys.alphaVantage });
 });
 
 const PORT = process.env.PORT || 5000;
