d67287e4201ff6fd9330f431038acba555bc88c1
cchak <chrischakct@gmail.com>
2018-12-05 20:03:37 -0500

    install body-praser, request

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
-const app = express();
 const keys = require("./config/keys");
+const request = require("request");
+const bodyParser = require("body-parser");
+const app = express();
 
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+app.post("/", function(req, res) {
+  const CHALLENGE = req.body.challenge;
+  res.send({ challenge: CHALLENGE });
+});
+//exchange
 app.get("/", (req, res) => {
-  res.send({ hi: keys.alphaVantage });
+  res.json({ hi: keys.alphaVantage });
 });
 
+//stock
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
